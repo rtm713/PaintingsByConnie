262ab5d54d225b7a2d35f49ce873600e0d5af1bf
rtm713 <rtm713@gmail.com>
2023-07-13 01:38:28 -0500

    updated styling, added photos, and mobile responsivness

---
+++ assets/logic.js
@@ -0,0 +1,20 @@
+
+var titleBox = document.querySelector('#headDiv');
+var titleLetters = [
+    'P', 'a', 'i', 'n', 't',
+    'i', 'n', 'g', 's', ' ',
+    'B', 'y', ' ', 'C', 'o',
+    'n', 'n', 'n', 'i', 'e'
+  ];
+var stop = 0;
+
+var titleAnimation = setInterval(function() {
+    var letter = document.createElement('h1');
+    letter.textContent = titleLetters[stop];
+    titleBox.append(letter);
+    stop++;
+
+    if (stop === titleLetters.length) {
+        clearInterval(titleAnimation);
+    };
+},200);
